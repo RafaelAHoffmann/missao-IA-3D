f772d2bb74dd7fca33642cf458724f6bdcb7a7d4
Rafael Hoffmann <hoffmann.rafael@escola.pr.gov.br>
2024-10-07 02:37:42 -0300

    update 0.3.3 - 2024.10.07 atualização: - perguntas, perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -21,7 +21,7 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "-Ei você! Você ouve a voz da pesquisadora chamando sua atenção. -Diga, você poderia participar de um questionário sobre racismo?",
+        enunciado: "\"Ei você!\" Você ouve a voz da pesquisadora chamando sua atenção. \"Diga, você poderia participar de um questionário sobre racismo?\"",
         alternativas: [
             {
                 texto: "Aceitar a proposta",
@@ -29,7 +29,7 @@ export const perguntas = [
                     "afirmacao 1",
                     "afirmacao 2"
                 ],
-                proxima: 1,
+                proxima: 3,
             },
             {
                 texto: "Recusar a proposta educadamente",
@@ -43,9 +43,30 @@ export const perguntas = [
                 texto: "Recusar a proposta.",
                 afirmacao: [
                     "Você ouve a voz da pesquisadora chamando sua atenção, ela pede para que Você participe da pesquisa, mas é recebida com um não. o tom de voz quando ela fala \"entendo, tenha um bom dia.\" diz que ela ja deve ter ouvido esse tipo de resposta varias vezes hoje.",
-                    "Você ouve a voz da pesquisadora chamando sua atenção. Você simplesmente finge não ter ouvido"
+                    "Você ouve a voz da pesquisadora chamando sua atenção. Você simplesmente finge não ter ouvido e continua no seu caminho."
                 ],
             }
+            
+        ]
+    },
+    {
+        enunciado: "\"Eu prometo, vai ser bem rápido, não se preocupe\" - a pesquisadora afirma. Você:",
+        alternativas: [
+            {
+                texto: "Aceita a proposta, no final das contas",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Recusa a proposta educadamente, mais uma vez",
+                afirmacao: [
+                    "A pesquisadora insiste em Você responder o questionário e, mais uma vez, Você recusa a proposta com educação. \"Ah, entendo. Tenha um bom dia\" - a pesquisadora diz para Você, antes de procurar outra pessoa para responder o questionário no seu lugar.",
+                    "Mais uma vez, a pesquisadora pede para que Você responda o questionário. Você explica com educação que não tem tempo agora. \"Entendo. Então, tenha um bom dia\" - ela diz antes de partir para a próxima pessoa."
+                ],
+            }           
             
         ]
     },
